3df17d42ec470a20551438b4c921cc4d9b8f6a6a
Subham Kedia <49448247+notsocommon15@users.noreply.github.com>
2020-05-02 05:30:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+new Vue({
+    el: ".tambola",
+    data() {
+      return {
+        boardNumbers: 90,
+        checked: [],
+        currentNumber: 0
+      };
+    },
+    computed: {
+      isFinished() {
+        return this.checked.length === 90;
+      }
+    },
+    methods: {
+      isChecked(n) {
+        return this.checked.includes(n+1);
+      },
+      pickNumber() {
+        while (1) {
+          let number = Math.ceil(Math.random() * this.boardNumbers);
+          if (!this.checked.includes(number)) {
+            this.checked.push(number);
+            this.currentNumber = number;
+            break;
+          }
+        }
+      }
+    }
+  });
+  
+  
